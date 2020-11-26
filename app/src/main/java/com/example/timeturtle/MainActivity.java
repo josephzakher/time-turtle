5ff04dd5ea5d3fd244ae4ebe5ab9657b71603e1a
Joseph Zakher <josephzakher@icloud.com>
2020-11-26 18:44:00 +0200

    Fixed margin of recyclerView.

--- app/src/main/java/com/example/timeturtle/MainActivity.java
+++ app/src/main/java/com/example/timeturtle/MainActivity.java
@@ -9,12 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.MenuItem;
-
 
 import com.google.android.material.navigation.NavigationView;
 
